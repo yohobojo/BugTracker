bc43bf9878b2c699308f2acba2d11f0de9e4c104
yohobojo <derek64johnson@gmail.com>
2022-10-31 15:28:34 -0600

    added new pages

---
+++ src/main.js
@@ -0,0 +1,8 @@
+$('#adminBtn').click(function (e) {
+  e.preventDefault();
+  window.location = 'adminHome.html';
+});
+$('#userBtn').click(function (e) {
+  e.preventDefault();
+  window.location = 'userHome.html';
+});
